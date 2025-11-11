165b767a679db53eb446272573a6907e0b4681bc
Nick <nikitosi3012@gmail.com>
2025-11-11 03:32:50 +0200

    Update FileNotFoundError exception

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-import os
-
-
 def copy_file(command: str) -> None:
     parts = command.split(" ")
     if len(parts) != 3 or parts[0] != "cp":
@@ -9,11 +6,11 @@ def copy_file(command: str) -> None:
     source_file = parts[1]
     file_copy = parts[2]
 
-    if not os.path.exists(source_file):
-        return
 
     if source_file == file_copy:
         return
-
-    with open(source_file, "r") as file_out, open(file_copy, "w") as file_in:
-        file_in.write(file_out.read())
+    try:
+        with open(source_file, "r") as file_out, open(file_copy, "w") as file_in:
+            file_in.write(file_out.read())
+    except FileNotFoundError:
+        return
