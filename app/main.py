ad6364af3c0dc02625304c17aff2d36abb5ee658
Nick <nikitosi3012@gmail.com>
2025-11-11 03:34:46 +0200

    Update flake8 requirements

--- app/main.py
+++ app/main.py
@@ -6,11 +6,12 @@ def copy_file(command: str) -> None:
     source_file = parts[1]
     file_copy = parts[2]
 
-
     if source_file == file_copy:
         return
+
     try:
-        with open(source_file, "r") as file_out, open(file_copy, "w") as file_in:
+        with (open(source_file, "r") as file_out,
+              open(file_copy, "w") as file_in):
             file_in.write(file_out.read())
     except FileNotFoundError:
         return
